da35faacd4cb5599fbeb22218db90ab1ab0355d1
David Zhu <jiaqizhu@ucsb.edu>
2023-06-27 12:23:21 -0700

    use fetch?

--- api/callback.js
+++ api/callback.js
@@ -1,12 +1,25 @@
 // api/callback.js
-import { kv } from "@vercel/kv";
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    const { code, state } = req.query;
+  const { code, state } = req.query;
+  const kvUrl = process.env.KV_URL;
 
-    // Save the auth code to Vercel KV
-    await kv.set(`auth_token_${state}`, code);
+  // Save the auth code to Vercel KV
+  const response = await fetch(`${kvUrl}/auth_code_${state}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.KV_REST_API_TOKEN}`,
+    },
+    body: JSON.stringify({ value: code }),
+  });
 
-    // Optionally, you can redirect the user to a success page
-    res.redirect('/success');
+  if (!response.ok) {
+    res.status(500).json({ error: 'Failed to save auth code' });
+    return;
+  }
+
+  // Optionally, you can redirect the user to a success page
+  res.redirect('/success');
 }
--- api/getauthcode.js
+++ api/getauthcode.js
@@ -1,22 +1,29 @@
 // api/getAuthCode.js
-import { kv } from "@vercel/kv";
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    const { state } = req.query;
-    const apiKey = req.headers['x-api-key'];
-    
-    // Check if the API key matches the one in the environment variables
-    if (apiKey !== process.env.API_KEY) {
-        res.status(403).json({ error: 'Invalid API key' });
-        return;
-    }
+  const { state } = req.query;
+  const apiKey = req.headers['x-api-key'];
+  const kvUrl = process.env.KV_URL;
 
-    // Retrieve the auth code from Vercel KV
-    const code = await kv.get(`auth_code_${state}`);
-    
-    if (code) {
-        res.status(200).json({ code });
-    } else {
-        res.status(404).json({ error: 'Auth code not found' });
-    }
+  // Check if the API key matches the one in the environment variables
+  if (apiKey !== process.env.API_KEY) {
+    res.status(403).json({ error: 'Invalid API key' });
+    return;
+  }
+
+  // Retrieve the auth code from Vercel KV
+  const response = await fetch(`${kvUrl}/auth_code_${state}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.KV_REST_API_READ_ONLY_TOKEN}`,
+    },
+  });
+
+  if (!response.ok) {
+    res.status(404).json({ error: 'Auth code not found' });
+    return;
+  }
+
+  const data = await response.json();
+  res.status(200).json({ code: data.value });
 }
