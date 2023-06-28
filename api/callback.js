87c09ac441469ccfa729d7518df34e91a3bebbc8
davidwuya <55730058+davidwuya@users.noreply.github.com>
2023-06-28 01:22:24 -0700

    finally done?

--- api/callback.js
+++ api/callback.js
@@ -1,35 +1,42 @@
-import { kv } from "@vercel/kv";
 import axios from 'axios';
+import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
+  // Get the authorization code and state from the query parameters.
   const { code, state } = req.query;
 
-  // Store the code and state to Vercel KV with 1-minute expiry.
-  await kv.set("authorization_data", { code, state }, { expires: new Date(Date.now() + 60000) });
+  if (state !== process.env.STATE) {
+    return res.status(401).send('Unauthorized');
+  }
 
   // Perform the OAuth2 token exchange.
-  const tokenResponse = await axios.post('https://api.digikey.com/v1/oauth2/token', {
-    code: code,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: 'https://your-vercel-app/api/oauth-callback',
-    grant_type: 'authorization_code'
-  }, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  });
+  try {
+    const tokenResponse = await axios.post('https://api.digikey.com/v1/oauth2/token', {
+      code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: 'authorization_code'
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 
-  // Check if the request was successful.
-  if (tokenResponse.status === 200) {
-    // Get the access token from the response.
-    const accessToken = tokenResponse.data.access_token;
+    let { access_token, refresh_token, expires_in, refresh_token_expires_in, token_type } = tokenResponse.data;
 
-    // Store the access token to Vercel KV.
-    await kv.set("access_token", accessToken, { expires: new Date(Date.now() + 60000) });
+    // Calculate the exact timestamp when the tokens will expire.
+    expires_in = Date.now() + expires_in * 1000;
+    refresh_token_expires_in = Date.now() + refresh_token_expires_in * 1000;
 
-    res.status(200).send('Access token stored successfully');
-  } else {
-    res.status(500).send('Failed to exchange authorization code for access token');
+    // Store the tokens and expiry times to Vercel KV.
+    await kv.set("tokens", {
+      access_token, refresh_token, expires_in, refresh_token_expires_in, token_type
+    }, { expires: new Date(refresh_token_expires_in) });
+
+    res.status(200).send('Tokens stored successfully');
+  } catch (error) {
+    console.error('Error during token exchange:', error);
+    res.status(500).send('Error during token exchange');
   }
 }
