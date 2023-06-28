296eac3de0cb2ec5cb9544307d8b072c83814d82
David Zhu <jiaqizhu@ucsb.edu>
2023-06-28 12:17:13 -0700

    more fault messages

--- api/callback.js
+++ api/callback.js
@@ -25,7 +25,8 @@ export default async function handler(req, res) {
     });
 
     if (!tokenResponse.ok) {
-      throw new Error('Token exchange failed');
+      const errorText = await tokenResponse.text();
+      throw new Error(`Token exchange failed with status ${tokenResponse.status}: ${errorText}`);
     }
 
     const tokenData = await tokenResponse.json();
