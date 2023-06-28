a4a410114314d20b2d32533bdaf0e98b28c7f9a3
David Zhu <jiaqizhu@ucsb.edu>
2023-06-28 12:14:54 -0700

    rewrite using fetch api

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import { kv } from "@vercel/kv";
-
 export default async function handler(req, res) {
   // Get the authorization code and state from the query parameters.
   const { code, state } = req.query;
@@ -8,22 +5,31 @@ export default async function handler(req, res) {
   if (state !== process.env.OAUTH_STATE) {
     return res.status(401).send('Unauthorized');
   }
+
   let redirect_uri = "https://".concat(process.env.VERCEL_URL, "/api/callback");
+
   // Perform the OAuth2 token exchange.
   try {
-    const tokenResponse = await axios.post('https://api.digikey.com/v1/oauth2/token', {
-      code,
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    }, {
+    const tokenResponse = await fetch('https://api.digikey.com/v1/oauth2/token', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
-      }
+      },
+      body: new URLSearchParams({
+        code,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      })
     });
 
-    let { access_token, refresh_token, expires_in, refresh_token_expires_in, token_type } = tokenResponse.data;
+    if (!tokenResponse.ok) {
+      throw new Error('Token exchange failed');
+    }
+
+    const tokenData = await tokenResponse.json();
+    let { access_token, refresh_token, expires_in, refresh_token_expires_in, token_type } = tokenData;
 
     // Calculate the exact timestamp when the tokens will expire.
     expires_in = Date.now() + expires_in * 1000;
